d1222c4344ac9701181758dd06262b912ec6e54d
skid <112231743+Skidy89@users.noreply.github.com>
2024-09-28 15:17:12 -0600

    Update index.ts

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -4,7 +4,6 @@ import { makeBusinessSocket as _makeSocket } from './business'
 
 // export the last socket layer
 const makeWASocket = (config: UserFacingSocketConfig) => (
-	//The type of this expression cannot be named without a 'resolution-mode' assertion
 	_makeSocket({
 		...DEFAULT_CONNECTION_CONFIG,
 		...config
