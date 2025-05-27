6b3965075a01776c28d47a0c75d0bde615d63b48
skidy89 <tutorialesyg310@gmail.com>
2025-05-27 00:09:27 -0600

    fix: adjust random bytes length in generateMessageID function

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -203,7 +203,8 @@ export const generateMessageIDV2 = (userId?: string): string => {
 }
 
 // generate a random ID to attach to a message
-export const generateMessageID = () => '3EB0' + randomBytes(18).toString('hex').toUpperCase()
+export const generateMessageID = () => '3EB0' + randomBytes(9).toString('hex').toUpperCase()
+
 
 export function bindWaitForEvent<T extends keyof BaileysEventMap>(ev: BaileysEventEmitter, event: T) {
 	return async(check: (u: BaileysEventMap[T]) => boolean | undefined, timeoutMs?: number) => {
