81b97e9521bc31a2f562c351d93214ba17cec17a
skidy89 <tutorialesyg310@gmail.com>
2025-07-28 12:43:13 -0600

    fix

--- src/Socket/groups.ts
+++ src/Socket/groups.ts
@@ -343,7 +343,7 @@ export const extractGroupMetadata = (result: BinaryNode) => {
 		subject: group.attrs.subject,
 		subjectOwner: group.attrs.s_o,
 		subjectTime: +group.attrs.s_t,
-		size: getBinaryNodeChildren(group, 'participant').length,
+		size: group.attrs.size ? +group.attrs.size : getBinaryNodeChildren(group, 'participant').length,
 		creation: +group.attrs.creation,
 		owner: group.attrs.creator ? jidNormalizedUser(group.attrs.creator) : undefined,
 		desc,
